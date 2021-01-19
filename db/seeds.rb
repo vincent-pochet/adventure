526ca83b7aa1537df1ea7778e76160aafab51306
Vincent Pochet <vincent.pochet@gmail.com>
2021-01-19 19:04:43 +0100

    Add Settings seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,12 @@ Day.delete_all
     day.content = "<h1>#{i + 1} Décembre</h1>"
   end
 end
+
+Setting.delete_all
+Setting.create!(key: 'app.title', value: 'Adventure calendar', locale: 'en')
+Setting.create!(key: 'app.introduction', value: '<p>Welcome to the adventure calendar!</p>', locale: 'en')
+Setting.create!(key: 'error.forbidden', value: 'Good try! :)', locale: 'en')
+Setting.create!(key: 'error.unauthorized', value: 'I think your are not supposed to be here...', locale: 'en')
+Setting.create!(key: 'error.not_found', value: 'Hmmm... We found nothing there...', locale: 'en')
+Setting.create!(key: 'error.embed.not_found', value: nil, locale: 'en')
+Setting.create!(key: 'error.embed.forbidden', value: nil, locale: 'en')

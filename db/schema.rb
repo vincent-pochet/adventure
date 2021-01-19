ecd5c03c8a62fbe5f59dbabd86bebfe79562403a
Vincent Pochet <vincent.pochet@gmail.com>
2021-01-19 10:50:29 +0100

    Add backend to serve locals

--- db/schema.rb
+++ db/schema.rb
@@ -10,25 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_19_182528) do
-
+ActiveRecord::Schema.define(version: 20_210_119_182_528) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "days", force: :cascade do |t|
-    t.integer "number", null: false
-    t.text "introduction"
-    t.text "content", null: false
-    t.string "locale", default: "en", null: false
-    t.index ["locale", "number"], name: "index_days_on_locale_and_number", unique: true
+  create_table 'days', force: :cascade do |t|
+    t.integer 'number', null: false
+    t.text 'introduction'
+    t.text 'content', null: false
+    t.string 'locale', default: 'en', null: false
+    t.index %w[locale number], name: 'index_days_on_locale_and_number', unique: true
   end
 
-  create_table "settings", force: :cascade do |t|
-    t.string "key", null: false
-    t.text "value", null: false
-    t.string "locale"
-    t.index ["key"], name: "index_settings_on_key"
-    t.index ["locale"], name: "index_settings_on_locale"
+  create_table 'settings', force: :cascade do |t|
+    t.string 'key', null: false
+    t.text 'value', null: false
+    t.string 'locale'
+    t.index ['key'], name: 'index_settings_on_key'
+    t.index ['locale'], name: 'index_settings_on_locale'
   end
 
+  create_table 'settings', force: :cascade do |t|
+    t.string 'key', null: false
+    t.text 'value', null: false
+  end
 end
